2358091ad2082acde8d0bc469598bace7493a06e
Savio Abuga <savioabuga@gmail.com>
2020-02-11 11:30:49 +0300

    Optmize bubble sort

--- udemy-masterclass/Sorting/01bubbleSort.js
+++ udemy-masterclass/Sorting/01bubbleSort.js
@@ -1,12 +1,18 @@
 function bubbleSort(arr) {
+
+    let noSwaps;
+
     for (let i = 0; i < arr.length; i++) {
+        noSwaps = true;
         for (let j = 0; j < arr.length - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 let temp = arr[j + 1];
                 arr[j + 1] = arr[j];
                 arr[j] = temp;
+                noSwaps = false;
             }
         }
+        if (noSwaps) break
     }
 
     return arr;

fc989aff55e1c987f555eb026dc271a94314e176
Savio Abuga <savioabuga@gmail.com>
2020-02-11 11:21:38 +0300

    Implement bubble sort

---
+++ udemy-masterclass/Sorting/01bubbleSort.js
@@ -0,0 +1,15 @@
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    return arr;
+}
+
+console.log(bubbleSort([90, -20, 100, 0, -2000]))

a497f210e86be406be1b54205cf8e24e1c7676a4
Savio Abuga <savioabuga@gmail.com>
2020-02-11 05:40:59 +0300

    Fix basic linear search

--- udemy-masterclass/Search/01linearSearch.js
+++ udemy-masterclass/Search/01linearSearch.js
@@ -1,9 +1,9 @@
-// This function accepts an array and a value and returns value if found else -1
+// This function accepts an array and a value and returns index of value if found else -1
 
 function linearSearch(arr, value) {
-    for (let element of arr) {
-        if (value === element) {
-            return value
+    for (let i = 0; i < arr.length; i++) {
+        if (value === arr[i]) {
+            return i
         }
     }
     return -1;

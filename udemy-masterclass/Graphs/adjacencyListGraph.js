9a593a05fba852c6fd41b0f64a7294eea7e524be
Savio Abuga <savioabuga@gmail.com>
2020-02-22 16:16:11 +0300

    Add adjacency list graph

---
+++ udemy-masterclass/Graphs/adjacencyListGraph.js
@@ -0,0 +1,24 @@
+class Graph {
+    constructor() {
+        this.adjacencyList = {}
+    }
+
+    addVertex(vertex) {
+        this.adjacencyList[vertex] = [];
+    }
+
+    addEdge(vertex1, vertex2) {
+        this.adjacencyList[vertex1].push(vertex2);
+        this.adjacencyList[vertex2].push(vertex1);
+    }
+
+    removeEdge(vertex1, vertex2) {
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(vertex => vertex !== vertex2)
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(vertex => vertex !== vertex1)
+    }
+
+    removeVertex(vertex){
+        this.adjacencyList[vertex].map(v => this.removeEdge(v, vertex));
+        delete this.adjacencyList[vertex]
+    }
+}
